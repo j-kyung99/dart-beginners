1357b0c9289c2e46e5b24ac9bfceaed2ea3f26c9
j-kyung99 <apfwnls14@naver.com>
2024-01-10 13:41:19 +0900

    #1.6 Constant Variables

--- main.dart
+++ main.dart
@@ -1,6 +1,4 @@
 void main() {
-  late final String name;
-  // late는 final이나 var 앞에 붙여줄 수 있는 수식어
-  // 초기 데이터 없이 변수를 선언할 수 있게 해줌
-  // 값을 넣기 전에는 접근하지 못함
+  const max_allowed_price = 120;
+  // const 변수들은 컴파일 때 평가됨
 }
