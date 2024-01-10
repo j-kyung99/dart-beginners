0af8c260f8137928892b0f3f2d980123073271f7
j-kyung99 <apfwnls14@naver.com>
2024-01-10 13:35:52 +0900

    #1.5 Late Variables

--- main.dart
+++ main.dart
@@ -1,5 +1,6 @@
 void main() {
-  final name = 'jk';
-  // final은 js의 const와 같음. 상수 역할
-  // final을 통해 한 번 정의된 변수는 변경될 수 없음!
+  late final String name;
+  // late는 final이나 var 앞에 붙여줄 수 있는 수식어
+  // 초기 데이터 없이 변수를 선언할 수 있게 해줌
+  // 값을 넣기 전에는 접근하지 못함
 }
