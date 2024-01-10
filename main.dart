9ddd0615eb642ec88c78974b22b7fbd4b6758848
j-kyung99 <apfwnls14@naver.com>
2024-01-10 13:27:04 +0900

    #1.4 Final Variables

--- main.dart
+++ main.dart
@@ -1,5 +1,5 @@
 void main() {
-  String? jk = 'jk';
-  // String 일수도, null 일수도 있게 하려면 ?를 붙임
-  jk = null;
+  final name = 'jk';
+  // final은 js의 const와 같음. 상수 역할
+  // final을 통해 한 번 정의된 변수는 변경될 수 없음!
 }
