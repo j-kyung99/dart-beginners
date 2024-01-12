42c07aa0ca0e34a5fdf91957d841ae1f92208ce3
j-kyung99 <apfwnls14@naver.com>
2024-01-12 15:31:14 +0900

    #2.2 String Interpolation

--- main.dart
+++ main.dart
@@ -1,13 +1,6 @@
 void main() {
-  var giveMeFive = true;
-  var numbers = [
-    1,
-    2,
-    3,
-    4,
-    if (giveMeFive) 5,
-    // collection if
-    // giveMeFive가 true면 numbers list에 5를 추가함
-  ];
-  // list의 마지막에 ,(쉼표)를 붙이면 자동으로 여러 줄로 포매팅됨
+  var name = 'jk';
+  var age = 10;
+  var greeting = "Hello everyone, my name is $name and I'm ${age + 2}";
+  print(greeting);
 }
