fadae78d69c51426c2f0a34dfe1d3bf27ff32757
j-kyung99 <apfwnls14@naver.com>
2024-01-12 16:11:29 +0900

    #2.4 Maps

--- main.dart
+++ main.dart
@@ -1,11 +1,10 @@
 void main() {
-  var oldFriends = ['jk', 'gang'];
-  var newFriends = [
-    'lewis',
-    'ralph',
-    'darren',
-    for (var friend in oldFriends) "✅ $friend",
-    // collecion for
-  ];
-  print(newFriends);
+  var player = {
+    'name': 'jk',
+    'xp': 19.99,
+    'superpower': false,
+  };
+  Map<List<int>, bool> players = {
+    [1, 2, 3, 4]: true,
+  }; // 이와 같이 복잡한 key나 value도 가능함
 }
