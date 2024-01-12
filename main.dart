45bef41da2195c7a35920c6af650774a7e3bc2f9
j-kyung99 <apfwnls14@naver.com>
2024-01-12 15:20:48 +0900

    #2.1 Lists

--- main.dart
+++ main.dart
@@ -1,10 +1,13 @@
 void main() {
-  String name = 'jk';
-  bool alive = true;
-  int age = 12;
-  double money = 69.99;
-
-  num x = 12;
-  x = 1.1;
-  // num은 정수도 되고, 실수도 됨
+  var giveMeFive = true;
+  var numbers = [
+    1,
+    2,
+    3,
+    4,
+    if (giveMeFive) 5,
+    // collection if
+    // giveMeFive가 true면 numbers list에 5를 추가함
+  ];
+  // list의 마지막에 ,(쉼표)를 붙이면 자동으로 여러 줄로 포매팅됨
 }
