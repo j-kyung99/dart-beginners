21f8ae321de32672e56351d849fc7ed62d688f3f
j-kyung99 <apfwnls14@naver.com>
2024-01-12 16:14:34 +0900

    #2.5 Sets

--- main.dart
+++ main.dart
@@ -1,10 +1,3 @@
 void main() {
-  var player = {
-    'name': 'jk',
-    'xp': 19.99,
-    'superpower': false,
-  };
-  Map<List<int>, bool> players = {
-    [1, 2, 3, 4]: true,
-  }; // 이와 같이 복잡한 key나 value도 가능함
+  var numbers = {1, 2, 3, 4};
 }
